8eab0108b3555059cbfb8b9936b2244bb9b7e10e
brunop27 <brunopinheiro715@gmail.com>
2025-02-23 17:17:06 -0300

    Import  and Add Projects component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/pages/Home'
 import Contact from './components/pages/Contact'
 import Company from './components/pages/Company'
 import NewProject from './components/pages/NewProject'
+import Projects from './components/pages/Projects'
 
 import Container from './components/layout/Container'
 import NavBar from './components/layout/NavBar'
@@ -15,6 +16,7 @@ function App() {
       <Container customClass="min-height">
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/projects" element={<Projects />} />
           <Route exact path="/company" element={<Company />} />
           <Route exact path="/contact" element={<Contact />} />
           <Route exact path="/newProject" element={<NewProject />} />
