31daf908157bad369d1c4f53469932e417610b31
brunop27 <brunopinheiro715@gmail.com>
2025-02-23 16:55:18 -0300

    bug fixed "Switch" to "Routes" and add navBar/footer component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from './components/pages/Home'
 import Contact from './components/pages/Contact'
 import Company from './components/pages/Company'
 import NewProject from './components/pages/NewProject'
+
 import Container from './components/layout/Container'
+import NavBar from './components/layout/NavBar'
+import Footer from './components/layout/Footer'
+
 function App() {
   return (
     <Router>
-      <ul>
-        <li>Home</li>
-        <li>Contato</li>
-        <li>Contato</li>
-        <li>Contato</li>
-      </ul>
-      <Switch>
-        <Container customClass="min-height">
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/company">
-            <Company />
-          </Route>
-          <Route exact path="/contact">
-            <Contact />
-          </Route>
-          <Route exact path="/newProject">
-            <NewProject />
-          </Route>
-        </Container>
-      </Switch>
+      <NavBar/> 
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/company" element={<Company />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/newProject" element={<NewProject />} />
+        </Routes>
+      </Container>
+      <Footer/>
     </Router>
   );
 }
