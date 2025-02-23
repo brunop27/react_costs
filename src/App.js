2d2a32f5cd51a32504e6c087bc33baf313275576
brunop27 <brunopinheiro715@gmail.com>
2025-02-22 23:26:33 -0300

    Add files and structure to project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+import Container from './components/layout/Container'
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+        <li>Contato</li>
+        <li>Contato</li>
+      </ul>
+      <Switch>
+        <Container customClass="min-height">
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/company">
+            <Company />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/newProject">
+            <NewProject />
+          </Route>
+        </Container>
+      </Switch>
+    </Router>
   );
 }
 
