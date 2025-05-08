6fa2dfbfd755841d4e37c5c9d0a015d201e881e7
brunop27 <brunopinheiro715@gmail.com>
2025-05-08 20:47:36 -0300

    Prepare for incluse component Dashboard

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,8 @@
 import {useLocation} from 'react-router-dom'
 import Message from '../layout/Message'
+import Container from '../layout/Container'
+import styles from './Projects.module.css'
+import LinkButton from '../layout/LinkButton'
 
 function Projects(){
 
@@ -10,9 +13,15 @@ function Projects(){
     }
 
     return (
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto"/>
+            </div>
             {inform && <Message type="success" msg={inform}/>}
+            <Container customClass="start">
+                <p>Projetos</p>
+            </Container>
         </div>
     )
 }
